3c91841c597677ee30712f845df44d01d1344ecd
SmallSparkle <a.kortikova@gmail.com>
2023-01-06 13:08:38 +0100

    Guess number refactoring js

--- 00-js-basics/app.js
+++ 00-js-basics/app.js
@@ -2,23 +2,14 @@ var secretNum = Math.ceil(Math.random() * 10);
 var tries = 0;
 
 function guessNum(num) {
-  while (tries <= 5) {
-    if (tries === 5) {
-      alert("No more tries");
-      break;
-    }
-    if (num != secretNum && tries < 5) {
-      alert("Wrong");
-      tries++;
-      console.log("Try N: " + tries);
-      break;
-    }
-    if (num === secretNum) {
-      alert("You won");
-      break;
-    }
-    if (tries === 5) {
-      alert("No more tries");
-    }
+  if (tries >= 5) {
+    return alert("No more tries. Game ower");
+  }
+  if (num === secretNum) {
+    alert("You won");
+    tries = 5;
+  } else {
+    alert("You not guessed");
+    tries++;
   }
 }
